b762d32402417600fad3f6d1568231dcaf255c1e
Florian Warzecha <flowa2000@gmail.com>
2018-02-05 17:09:44 +0100

    jetbrains notnull anotation entfernt

--- src/main/java/florian/simpleJavaObfuscator/util/run/ObfuscationTypeParam.java
+++ src/main/java/florian/simpleJavaObfuscator/util/run/ObfuscationTypeParam.java
@@ -2,7 +2,6 @@ package florian.simpleJavaObfuscator.util.run;
 
 import com.github.liketechnik.ArgumentTypes;
 import com.github.liketechnik.Parameter;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Florian Warzecha
@@ -11,31 +10,26 @@ import org.jetbrains.annotations.NotNull;
  */
 public class ObfuscationTypeParam extends Parameter {
     
-    @NotNull
     @Override
     public String getId() {
         return "obfuscationType";
     }
     
-    @NotNull
     @Override
     public String getName() {
         return "obfuscationType";
     }
     
-    @NotNull
     @Override
     public String getShortName() {
         return "oT";
     }
     
-    @NotNull
     @Override
     public Object getDefaultValue() {
         return "file";
     }
     
-    @NotNull
     @Override
     public ArgumentTypes getType() {
         return ArgumentTypes.STRING;
